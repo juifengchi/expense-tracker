2201f4e517207059eff81b7c8e1528f5f3aca2fc
ravenera0317 <r6090408@gmail.com>
2021-07-21 16:34:29 +0800

    Add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -16,7 +21,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
