38276b39bfef87feeadde63d943b6775ac5508df
ravenera0317 <r6090408@gmail.com>
2021-07-21 18:52:53 +0800

    Add revise record function

--- app.js
+++ app.js
@@ -18,7 +18,17 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+    helpers: {
+      isEqual: function (x, y) {
+        return x === y
+      }
+    }
+  })
+)
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
@@ -37,6 +47,29 @@ app.get('/records/new', (req, res) => {
 app.post('/records', (req, res) => {
   const { name, date, category, amount } = req.body
   Record.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => console.error(error))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      record.save()
+    })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
