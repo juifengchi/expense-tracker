4fe4d1c96f971fdb21d02ebdd8be58f7857d4f9b
ravenera0317 <r6090408@gmail.com>
2021-07-22 17:00:44 +0800

    Modify date showing in edit page

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -3,31 +3,31 @@ const Record = require('../record')
 const record = [
   {
     name: '午餐',
-    date: '2019/04/23',
+    date: '2019-04-23',
     category: '餐飲食品',
     amount: 60
   },
   {
     name: '晚餐',
-    date: '2019/04/23',
+    date: '2019-04-23',
     category: '餐飲食品',
     amount: 60
   },
   {
     name: '捷運',
-    date: '2019/04/23',
+    date: '2019-04-23',
     category: '交通出行',
     amount: 120
   },
   {
     name: '電影：驚奇隊長',
-    date: '2019/04/23',
+    date: '2019-04-23',
     category: '休閒娛樂',
     amount: 220
   },
   {
     name: '租金',
-    date: '2019/04/01',
+    date: '2019-04-01',
     category: '家居物業',
     amount: 25000
   }
